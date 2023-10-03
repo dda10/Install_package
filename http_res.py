f5f3513f49569435d92d5af3ec0e180b3fd6f88a
dda10 <63626071+dda10@users.noreply.github.com>
2023-10-03 19:11:09 +0700

    Create http_res.py

---
+++ http_res.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python3
+import requests
+import datetime
+import time
+
+http_response_map = {
+    200: "Success",
+    201: "Created",
+    204: "No Content",
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    500: "Internal Server Error",
+}
+def check_website(url):
+    try:
+        start_time = datetime.datetime.now()
+        response = requests.get("http://"+url)
+        end_time = datetime.datetime.now()
+
+        status_code = response.status_code
+        response_time = (end_time - start_time).total_seconds()
+        content_length = len(response.content)
+
+        result = http_response_map.get(status_code, "Unknown")
+        timestamp = int(time.time())
+
+        return result, status_code, response_time, content_length, timestamp
+    except Exception as e:
+        return "Error", 0, 0, 0, datetime.datetime.now()
+
+# Read the list of websites from a file
+with open('/opt/domain', 'r') as file:
+    websites = file.read().splitlines()
+
+# Iterate through the list of websites and check their responses
+for website in websites:
+    result, status_code, response_time, content_length, timestamp = check_website(website)
+
+    content_length = int(content_length)
+    http_response_code = int(status_code)
+    result_type ='"' + result + '"'
+    response_time = float(response_time)
+    print(
+    "https_response,"
+    "method=GET,"
+    f"result={result},"
+    f"server={website},"
+    f"status_code={status_code} "
+    f"content_length={content_length},"
+    f"http_response_code={http_response_code},"
+    f"response_time={response_time},"
+    f"result_type={result_type}"
+    )
+
