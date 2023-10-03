e0299b092bb713daeaa058dae4c125505bf0cfa7
dda10 <63626071+dda10@users.noreply.github.com>
2023-10-03 19:08:53 +0700

    Update dns.py

--- dns.py
+++ dns.py
@@ -10,14 +10,12 @@ with open('/opt/domain', 'r') as file:
 def perform_dns_lookup(domain):
     try:
         timestamp = int(time.time())
-        ip_addresses = socket.gethostbyname_ex(domain)
-        ip_list = ', '.join(ip_addresses[2])
-        print(f"dns_record,domain={domain},ip={ip_list} {timestamp}")
+        ip =  socket.gethostbyname(domain)
+        ip = '"' + ip + '"'
+        domain = str(domain)
+        print("dns_record,domain=" + domain + " ip=" + ip  )
     except socket.gaierror as e:
         print(f"Error looking up IP for {domain}: {e}")
-
-# List of domains to perform DNS lookup on
-#domains = ["example.com", "google.com", "github.com"]  # Add as many domains as needed
 
 # Create a thread for each domain and start them
 threads = []
